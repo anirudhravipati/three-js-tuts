13a5687ee25c38e38c1e62deaa9d8aa1d545bc58
Aniruddh Ravipati <ravipatianirudh@gmail.com>
2023-01-08 16:14:46 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+import javascriptLogo from './public/javascript.svg'
+
+document.querySelector('#app').innerHTML = `
+    <h1>Hello Vite!</h1>
+`
+
+setupCounter(document.querySelector('#counter'))
